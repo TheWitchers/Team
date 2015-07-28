09b2f17d1f7aa23063ee6c6612cae23157a6e189
dvir <dviryamin1@gmail.com>
2015-07-28 12:13:59 +0300

    tests. wrappr socket for server-client connection

---
+++ TestingArea/Insering_Data.py
@@ -0,0 +1,29 @@
+__author__ = 'dvir'
+import sqlite3
+import tkFileDialog
+#a = tkFileDialog.askopenfilename()
+conn = sqlite3.connect("C:/Users/dvir/PycharmProjects/Team/DataBase/TeamDB.db")
+#print a
+c = conn.cursor()
+
+a = "y"
+while a == "y":
+    c.execute("INSERT INTO Users VALUES ('"
+              + raw_input("Enter nickname: ") + "', '"
+              + raw_input("Enter email: ") + "', '"
+              + raw_input("Enter first name: ") + "', '"
+              + raw_input("Enter last name: ") + "', '"
+              + raw_input("Enter username: ") + "', '"
+              + raw_input("Enter password: ") + "', "
+                                                "'false','"
+              + raw_input("Enter verefication question: ") + "', '"
+              + raw_input("Enter answer: ") + "')")
+
+    # c.execute("INSERT INTO 'Users' VALUES ('dvirking', 'dviryamin@gmail.com', 'dvir', 'yamin', 'dviryamin', 'schrhnhi','false','some', 'some')")
+    a = raw_input("continue? ('y' to proceed)")
+for row in c.execute("SELECT * FROM Users"):
+        print row
+
+a = raw_input("save changes? ('y' to save)")
+if a == "y":
+    conn.commit()
---
+++ TestingArea/dvir_methods_suggs.py
@@ -0,0 +1,27 @@
+__author__ = 'dvir'
+import tkFileDialog
+import sqlite3
+
+conn = sqlite3.connect("C:\Users\dvir\PycharmProjects\Team\DataBase\TeamDB.db")
+c = conn.cursor()
+
+
+def has_inf(col, tbl, info):
+    l = []
+    for row in c.execute(
+         "SELECT * FROM Users WHERE '" + col + "' = '" + info + "'"):
+        l.append(row)
+    if len(l) > 0:
+        print info + " already exists"
+        return True
+    else:
+        print info + " is OK"
+        return False
+
+
+def extract_inf(col, tbl, info):
+    if has_inf(col, tbl, info):
+        return str(0 in c.execute("SELECT * FROM Users WHERE '"+col+"' = '" + str(info) + "'"))
+    else:
+        print "info does not exist"
+        return ""
