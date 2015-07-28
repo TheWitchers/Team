a2e4ec49cc564adb1df1ac403fa6734fc4f7ebb4
dvir <dviryamin1@gmail.com>
2015-07-28 15:16:38 +0300

    credetials example

--- Credentials.py
+++ Credentials.py
@@ -1,3 +1,8 @@
 __author__ = 'dvir'
-class register:
-    pass
+from dvir_methods_suggs import *
+class Credentials:
+    def register(self, use, pas, email, fname, lname, nick, ver, ans):
+        if not has_inf("username", use) and not has_inf("email", email) and not has_inf("nickname", nick):
+            c.execute(
+                "INSERT INTO Users VALUES ('"+nick+"','"+email+"','"+fname+"','"+lname+"','"+use+"','"+pas+"', False,'"+ver+"','"+ans+"')")
+
--- dvir_methods_suggs.py
+++ dvir_methods_suggs.py
@@ -6,13 +6,13 @@ conn = sqlite3.connect("C:\Users\dvir\PycharmProjects\Team\DataBase\TeamDB.db")
 c = conn.cursor()
 
 
-def has_inf(col, tbl, info):
+def has_inf(col, info):
     if len(c.execute(
-         "SELECT '" + col + "' FROM '" + tbl + "' WHERE '"+col+"' = '" + id + "' AND '" + col + "' = '" + info + "'")) > 0:
-        print col + " already exists"
+         "SELECT '" + col + "' FROM Users WHERE  '" + col + "' = '" + info + "'")) > 0:
+
         return True
     else:
-        print col + " is OK"
+
         return False
 
 
