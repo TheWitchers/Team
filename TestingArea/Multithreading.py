368ac35bfe5deecd1d54141ed97408ff3433ad9d
dvir <dviryamin1@gmail.com>
2015-08-26 23:55:18 +0300

    multithreading learning examples

---
+++ TestingArea/Multithreading.py
@@ -0,0 +1,25 @@
+__author__ = 'dvir'
+import threading, random
+
+def Splitter(words):
+    list = words.split( )
+    nlist=[]
+    while(list):
+        nlist.append(list.pop(random.randrange(0,len(list))))
+
+    print(' '.join(nlist))
+
+if __name__=='__main__':
+    sen = 'Your god damn right.'
+    numOfTreads = 5
+    threadList = []
+
+    print("STARTING...\n")
+    for i in range(numOfTreads):
+        t=threading.Thread(target=Splitter,
+                           args=(sen,))
+        t.start()
+        threadList.append(t)
+
+    print("\nTread Count: "+str(threading.activeCount()))
+    print("EXITING...\n")
---
+++ TestingArea/Multithreading2.py
@@ -0,0 +1,39 @@
+__author__ = 'dvir'
+import Queue,threading,time,random
+
+class Producer:
+    def __init__(self):
+        self.food = ["salad","rice", "steak"]
+        self.nextTime = 0
+    def run(self):
+        global q
+        while (time.clock()<10):
+            if (self.nextTime<time.clock()):
+                f= self.food[random.randrange(len(self.food))]
+            q.put(f)
+            print ("Adding " + f)
+            self.nextTime+=random.random()
+
+class Consumer():
+    def __init__(self):
+        self.nextTime = 0
+    def run(self):
+        global q
+        while (time.clock()<10):
+                if (self.nextTime<time.clock() and not q.empty()):
+                    f=q.get()
+                    print("Removeing "+f)
+                    self.nextTime+=random.random()*2
+        print("EXITING...\n")
+
+if __name__=='__main__':
+
+    print("STARTING...\n")
+    q=Queue.Queue(10)
+
+    p=Producer()
+    c=Consumer()
+    pt=threading.Thread(target=p.run,args=())
+    ct=threading.Thread(target=c.run,args=())
+    pt.start()
+    ct.start()
